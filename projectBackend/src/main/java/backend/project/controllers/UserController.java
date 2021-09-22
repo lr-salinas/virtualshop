dc53fb9fbc615b91b7cb83a5637009707f37f4c6
coriccf <corina.colque.f@gmail.com>
2021-09-21 23:03:52 -0400

    Fixing User Controller

--- projectBackend/src/main/java/backend/project/controllers/UserController.java
+++ projectBackend/src/main/java/backend/project/controllers/UserController.java
@@ -5,6 +5,7 @@ import backend.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.rmi.ServerException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,13 +23,38 @@ public class UserController {
         return userService.listUser();
     }
 
+
+
+
     //GET para la obtencion de datos del usuario por id
     @GetMapping("/{userid}")
     public Optional<User> getByUserId(@PathVariable Integer userid){
         return (Optional<User>) userService.getByUserId(userid);
     }
 
-    //Creacion  de usuarios
+
+    //	AGREGAR UN NUEVO USUARIO
+    @PostMapping
+    public User guardarUser(@RequestBody User user) throws ServerException {
+        System.out.println(user);
+        userService.guardar(user);
+        if (user == null) {
+            throw new ServerException(null);
+        }
+        else {
+            return user;
+        }
+    }
+
+
+    //	ACTUALIZAR UN USUARIO
+    @PutMapping
+    public void actualizarUser(@RequestBody User user) {
+        userService.actualizar(user);
+    }
+
+
+    /*//AGREGAR NUEVO USUARIO
     @PostMapping
     public List<User> userCreate(){
         return userService.userCreation();
@@ -39,5 +65,7 @@ public class UserController {
     public Optional<User> userUpdate(@PathVariable Integer userid) {
         return (Optional<User>) userService.userUpdate(userid);
     }
+
+     */
 
 }
